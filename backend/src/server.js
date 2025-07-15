64ab5505fa3d9a12dbc27b0ba700c65829516923
Sujit <sujit@codewave.com>
2025-07-15 15:20:14 +0530

    startServer Function added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,8 +4,18 @@ import { connectDB } from "./config/db.js";
 
 const app = express();
 
-connectDB();
+app.get("/", (_, res) => res.send("Hello from server"));
 
-app.get("/", (req, res) => res.send("Hello from server"));
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () =>
+      console.log("Server is running on PORT:", ENV.PORT)
+    );
+  } catch (error) {
+    console.log("Failed to start server:", error);
+    process.exit(1);
+  }
+};
 
-app.listen(ENV.PORT, () => console.log("Server is running on PORT:", ENV.PORT));
+startServer();
